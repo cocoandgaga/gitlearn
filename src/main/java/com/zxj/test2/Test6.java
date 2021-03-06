b18ac76e44cdf044d5eddbb83d7d320dc44fee82
cocoandgaga <1004027030@qq.com>
2021-03-06 17:44:45 +0800

    '-am'

--- src/main/java/com/zxj/test2/Test6.java
+++ src/main/java/com/zxj/test2/Test6.java
@@ -2,6 +2,10 @@ package com.zxj.test2;
 
 public class Test6 {
     public static void main(String[] args) {
+        System.out.println("dddddd");
         System.out.println("diff");
+        System.out.println("ddd:");
+        System.out.println("git add");
+        System.out.println("fdfdf");
     }
 }
