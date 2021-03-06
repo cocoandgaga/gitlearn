0194a72726bb44de3554bc7e698998a712ff506c
cocoandgaga <1004027030@qq.com>
2021-03-06 22:39:14 +0800

    git cheout --

--- src/main/java/com/zxj/test2/CheckoutTestBranch.java
+++ src/main/java/com/zxj/test2/CheckoutTestBranch.java
@@ -4,5 +4,6 @@ public class CheckoutTestBranch {
     public static void main(String[] args) {
         System.out.println("checkout test");
         System.out.println("reset");
+        System.out.println("git checkout -- xxxx.java/xxx.html/...");
     }
 }
